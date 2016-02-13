78b9bd68ae6d38d8c5b084a75c268ef2599ced8d
Samba Tiyyagura <samba.tiyyagura@ypg.com>
2016-02-13 17:02:38 -0500

    Initial Commit

---
+++ TouchUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchUp
+//
+//  Created by Samba Tiyyagura on 2016-02-13.
+//  Copyright © 2016 Samba Tiyyagura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
